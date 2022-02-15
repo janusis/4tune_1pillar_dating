29f9bd3df381fec2af287a06a79aec1b654d88d5
jo <uuu9214@naver.com>
2022-02-15 09:58:24 +0900

    Initial commit

---
+++ src/main/java/sajudating/jpadating/domain/Member.java
@@ -0,0 +1,16 @@
+package sajudating.jpadating.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+
+}
---
+++ src/test/java/sajudating/jpadating/JpadatingApplicationTests.java
@@ -0,0 +1,13 @@
+package sajudating.jpadating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpadatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
