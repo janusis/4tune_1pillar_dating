27da58e91da5721af51250ff55eb36bb516dcc62
jo <uuu9214@naver.com>
2022-03-23 20:17:35 +0900

    memberDTO에 각 필드별 어노테이션 수정
    @Notblank, @NotEmpty, @Email, @Pattern 등등

--- src/main/java/sajudating/jpadating/DTO/MemberDTO.java
+++ src/main/java/sajudating/jpadating/DTO/MemberDTO.java
@@ -5,8 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 import sajudating.jpadating.domain.Gender;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Getter
@@ -15,13 +18,23 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class MemberDTO {
 
+    @NotBlank(message = "아이디에 공백이 포함 되어 있습니다.")
     private String userId;
+    @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
     private String pw;
+    @NotBlank(message = "이름에 공백이 포함 되어 있습니다.")
     private String name;
+    @NotEmpty
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
+    @NotBlank(message = "올바른 휴대전화 번호가 아닙니다")
+    @Pattern(regexp = "^01(0|1|[6-9])-(\\d{3}|\\d{4})-\\d{4}$",message = "올바른 휴대전화 형식이 아닙니다.")
     private String phone;
+    @Past
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
     private String birthTime;
+    @NotBlank
     private String nickname;
     public Gender gender;
 
